92844f26e7720f48fb61fb510b072b476572366a
MostafaMoh08 <92910644+MostafaMoh08@users.noreply.github.com>
2022-12-02 20:38:41 -0800

    Add files via upload

---
+++ Lecture_1/ASS3/main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+void main()
+{
+
+int a;
+int b;
+int sum;
+int sub;
+
+
+
+
+printf("please enter number a: ");
+scanf("%d",&a);
+
+printf("please enter number b: ");
+scanf("%d",&b);
+
+
+sum=a+b;
+
+sub=a-b;
+
+
+    printf("a+b = %d \n",sum);
+
+    printf("a-b = %d \n",sub);
+
+    printf("a&b = %d \n",a&b);
+
+    printf("a|b = %d \n",a|b);
+
+    printf("a^b = %d",a^b);
+
+
+
+
+}
+
+
+
+
